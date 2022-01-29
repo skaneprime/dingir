d4156ef9c23f3f2a7ba6db8c821a3f3bfe8fae34
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-01-30 00:21:19 +0300

    Fix: Non Digit Symbols

--- src/compiler/coder/key.ts
+++ src/compiler/coder/key.ts
@@ -1,6 +1,8 @@
 import { env, Utils } from '../../dingir';
 import { Bond } from '../../helper/bond';
 
-export const GET_SECRET_KEY = Bond(() =>
-  Array.from(`${Utils.String.hashCode(env.version)}-AttackOnTitan-ErenYeager`).map(Number),
-);
+export const GET_SECRET_KEY = Bond(() => {
+  const hash = `${env.version}-${process.versions.node}`;
+
+  return `${Utils.String.hashCode(hash)}`.split('').map(Number);
+});
