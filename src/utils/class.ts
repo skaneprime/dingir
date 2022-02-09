8e85b4a8e2e52a23906a9350b9dd70d619bb68b8
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-02-09 15:32:15 +0300

    Added Unknow type

--- src/utils/class.ts
+++ src/utils/class.ts
@@ -1,5 +1,8 @@
 /** @public */
-export type Any<T extends unknown[] = unknown[], I = unknown> = new (...args: T) => I;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Any<T extends any[] = any[], I = any> = new (...args: T) => I;
+/** @public */
+export type Unknown<T extends unknown[] = unknown[], I = unknown> = new (...args: T) => I;
 /** @public */
 export function isClass(obj: unknown) {
 	return /^\s*class[^\w]+/.test(obj + "");
--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,5 +1,8 @@
 /** @public */
-export type Any<T extends unknown[] = unknown[], R = unknown> = (...args: T) => R;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Any<T extends any[] = any[], R = any> = (...args: T) => R;
+/** @public */
+export type Unknown<T extends unknown[] = unknown[], R = unknown> = (...args: T) => R;
 /** @public */
 export function extend<F extends Any, D, M = { [P in keyof D]: D[P] }>(Function: F, DataMap?: M) {
 	return Object.assign(Function, DataMap);
