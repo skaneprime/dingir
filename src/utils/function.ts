4e14767c70371caca850e45ea9cb6586251f5fa9
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-02-24 22:12:20 +0300

    combiner function for summing multiple functions into one body

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,9 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 /** @public */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Any<T extends any[] = any[], R = any> = (...args: T) => R;
 /** @public */
 export type Unknown<T extends unknown[] = unknown[], R = unknown> = (...args: T) => R;
 /** @public */
 export function extend<F extends Any, D, M = { [P in keyof D]: D[P] }>(Function: F, DataMap?: M) {
 	return Object.assign(Function, DataMap);
+}
+/** @public */
+export function combiner(...combinerArgs: any[]) {
+	return (...funcs: Any[]) => {
+		return (...args: any[]) =>
+			funcs.forEach(async (func) => {
+				await func(...combinerArgs, ...args);
+			});
+	};
 }
