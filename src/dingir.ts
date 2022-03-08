dee623f118cf86ae446c1c2c2a8b4d314f98ba1d
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-03-09 01:11:10 +0300

    Moved env for external use to seperate file

--- src/dingir.ts
+++ src/dingir.ts
@@ -1,10 +1,6 @@
 import * as Compiler from "./compiler";
 import * as Utils from "./utils";
 import * as Logger from "./services/logger";
-
-/** @public */
-const env = {
-	version: "0.9.0",
-};
+import * as env from "./env";
 
 export { Compiler, Utils, Logger, env };
