a84d9f4b9020953824ec34d228e5fbe8d8cd3350
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-04-13 01:42:42 +0300

    A little tweaking with path (Error fix)

--- src/cli/install.ts
+++ src/cli/install.ts
@@ -4,16 +4,17 @@ import { systemLogger } from "../services/logger/system";
 import api from "../server/api";
 import { existsSync } from "fs";
 
-const PATH_TO_PAKAGE_JSON = `${process.cwd()}/package.json`;
-const readPkgJson = () => readFile(PATH_TO_PAKAGE_JSON, 'utf8');
+const readPkgJson = () => readFile(`${process.cwd()}/package.json`, "utf8");
 
 async function readLibsFromPKGJson() {
 	const packageJsonFile = await readPkgJson();
 	const json = JSON.parse(packageJsonFile);
-	
-	return json?.dingir?.libraries as {
-		[key: string]: string
-	} | undefined;
+
+	return json?.dingir?.libraries as
+		| {
+				[key: string]: string;
+		  }
+		| undefined;
 }
 
 program
@@ -23,55 +24,53 @@ program
 	.action(async (library: string | undefined, options: { debug?: boolean | string }) => {
 		const libraries: string[] = library ? [library] : [];
 		const libFolder = `${process.cwd()}/dingir_libs`;
-		
-		if(!library) {
+
+		if (!library) {
 			const pkgLibs = await readLibsFromPKGJson();
-			for(const libName in pkgLibs) {
+			for (const libName in pkgLibs) {
 				libraries.push(`${libName}@${pkgLibs[libName]}`);
 			}
 		}
-		
+
 		enableDebugIfTrue(options);
-		
-		for(const library of libraries) {
+
+		for (const library of libraries) {
 			systemLogger.debug(`[Install Lib]: searching ${library}`);
 
 			const [lib, ver] = library.split("@");
 			const fetchedLib = await api.libs.getLib(lib, ver);
-	
+
 			systemLogger.debug(`[Install Lib]: found ${lib}@${fetchedLib.version}`);
-	
+
 			if (!existsSync(libFolder)) {
 				systemLogger.debug(`[Install Lib]: creating lib folder`);
 				await mkdir(libFolder);
 			}
-	
+
 			if (fetchedLib.file) {
 				systemLogger.debug(`[Install Lib]: writing ${lib}@${fetchedLib.version}.dg`);
 				await writeFile(`${libFolder}/${lib}@${fetchedLib.version}.dg`, fetchedLib.file);
 			}
-	
+
 			if (fetchedLib.decl) {
 				systemLogger.debug(`[Install Lib]: writing ${lib}@${fetchedLib.version}.d.ts`);
 				await writeFile(`${libFolder}/${lib}@${fetchedLib.version}.d.ts`, fetchedLib.decl);
 			}
-	
+
 			systemLogger.info(`[Install Lib]: ${lib}@${fetchedLib.version} was installed!`);
 			const pkgJson = JSON.parse(await readPkgJson());
-			
-			if(pkgJson.dingir)
-				if(pkgJson.dingir.libraries) 
-					pkgJson.dingir.libraries[lib] = fetchedLib.version;
-				else 
-					pkgJson.dingir.libraries = { [lib]: fetchedLib.versions }; 
-			else 
+
+			if (pkgJson.dingir)
+				if (pkgJson.dingir.libraries) pkgJson.dingir.libraries[lib] = fetchedLib.version;
+				else pkgJson.dingir.libraries = { [lib]: fetchedLib.versions };
+			else
 				pkgJson.dingir = {
 					libraries: {
-						[lib]: fetchedLib.version
-					}
-				}
+						[lib]: fetchedLib.version,
+					},
+				};
 
-			await writeFile(PATH_TO_PAKAGE_JSON, JSON.stringify(pkgJson, null, '  '));
+			await writeFile(`${process.cwd()}/package.json`, JSON.stringify(pkgJson, null, "  "));
 			systemLogger.debug(`[Install Lib]: package.json was updated`);
 		}
 	});
