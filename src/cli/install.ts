9307bedb9bc743f0fa976022d6d345c1803ec3fb
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-04-13 01:31:56 +0300

    Install wil now resolve libs from package json

--- src/cli/install.ts
+++ src/cli/install.ts
@@ -1,38 +1,77 @@
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, writeFile, readFile } from "fs/promises";
 import { enableDebugIfTrue, program } from ".";
 import { systemLogger } from "../services/logger/system";
 import api from "../server/api";
 import { existsSync } from "fs";
 
+const PATH_TO_PAKAGE_JSON = `${process.cwd()}/package.json`;
+const readPkgJson = () => readFile(PATH_TO_PAKAGE_JSON, 'utf8');
+
+async function readLibsFromPKGJson() {
+	const packageJsonFile = await readPkgJson();
+	const json = JSON.parse(packageJsonFile);
+	
+	return json?.dingir?.libraries as {
+		[key: string]: string
+	} | undefined;
+}
+
 program
-	.command("install <library>")
-	.description("Installs existing library from server")
+	.command("install [library]")
+	.description("Installs existing library from server or from package.json")
 	.option("--debug [mode]")
-	.action(async (library: string, options: { debug?: boolean | string }) => {
+	.action(async (library: string | undefined, options: { debug?: boolean | string }) => {
+		const libraries: string[] = library ? [library] : [];
 		const libFolder = `${process.cwd()}/dingir_libs`;
+		
+		if(!library) {
+			const pkgLibs = await readLibsFromPKGJson();
+			for(const libName in pkgLibs) {
+				libraries.push(`${libName}@${pkgLibs[libName]}`);
+			}
+		}
+		
 		enableDebugIfTrue(options);
+		
+		for(const library of libraries) {
+			systemLogger.debug(`[Install Lib]: searching ${library}`);
 
-		systemLogger.debug(`[install lib]: searching ${library}`);
+			const [lib, ver] = library.split("@");
+			const fetchedLib = await api.libs.getLib(lib, ver);
+	
+			systemLogger.debug(`[Install Lib]: found ${lib}@${fetchedLib.version}`);
+	
+			if (!existsSync(libFolder)) {
+				systemLogger.debug(`[Install Lib]: creating lib folder`);
+				await mkdir(libFolder);
+			}
+	
+			if (fetchedLib.file) {
+				systemLogger.debug(`[Install Lib]: writing ${lib}@${fetchedLib.version}.dg`);
+				await writeFile(`${libFolder}/${lib}@${fetchedLib.version}.dg`, fetchedLib.file);
+			}
+	
+			if (fetchedLib.decl) {
+				systemLogger.debug(`[Install Lib]: writing ${lib}@${fetchedLib.version}.d.ts`);
+				await writeFile(`${libFolder}/${lib}@${fetchedLib.version}.d.ts`, fetchedLib.decl);
+			}
+	
+			systemLogger.info(`[Install Lib]: ${lib}@${fetchedLib.version} was installed!`);
+			const pkgJson = JSON.parse(await readPkgJson());
+			
+			if(pkgJson.dingir)
+				if(pkgJson.dingir.libraries) 
+					pkgJson.dingir.libraries[lib] = fetchedLib.version;
+				else 
+					pkgJson.dingir.libraries = { [lib]: fetchedLib.versions }; 
+			else 
+				pkgJson.dingir = {
+					libraries: {
+						[lib]: fetchedLib.version
+					}
+				}
 
-		const [lib, ver] = library.split("@");
-		const fetchedLib = await api.libs.getLib(lib, ver);
-
-		systemLogger.debug(`[install lib]: found ${lib}@${fetchedLib.version}`);
-
-		if (!existsSync(libFolder)) {
-			systemLogger.debug(`[install lib]: creating lib folder`);
-			await mkdir(libFolder);
+			await writeFile(PATH_TO_PAKAGE_JSON, JSON.stringify(pkgJson, null, '  '));
+			systemLogger.debug(`[Install Lib]: package.json was updated`);
 		}
-
-		if (fetchedLib.file) {
-			systemLogger.debug(`[install lib]: writing ${lib}@${fetchedLib.version}.dg`);
-			await writeFile(`${libFolder}/${lib}@${fetchedLib.version}.dg`, fetchedLib.file);
-		}
-
-		if (fetchedLib.decl) {
-			systemLogger.debug(`[install lib]: writing ${lib}@${fetchedLib.version}.d.ts`);
-			await writeFile(`${libFolder}/${lib}@${fetchedLib.version}.d.ts`, fetchedLib.decl);
-		}
-
-		systemLogger.info(`[install lib]: ${lib}@${fetchedLib.version} was installed!`);
 	});
